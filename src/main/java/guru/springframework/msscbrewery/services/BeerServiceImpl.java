1d69b7f911bb90ef176d98a394463873ab258c32
Smit Patel <smit.patel@hitachivantara.com>
2023-03-15 21:05:02 +0530

    lombok and mapstruct

--- src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -1,6 +1,7 @@
 package guru.springframework.msscbrewery.services;
 
 import guru.springframework.msscbrewery.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -8,13 +9,30 @@ import java.util.UUID;
 /**
  * Created by jt on 2019-04-20.
  */
+
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
     @Override
     public BeerDto getBeerById(UUID beerId) {
-        return BeerDto.builder().id(UUID.randomUUID())
+        return BeerDto.builder().id(beerId)
                 .beerName("Galaxy Cat")
                 .beerStyle("Pale Ale")
                 .build();
+    }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return BeerDto.builder().id(UUID.randomUUID()).build();
+    }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDto beerDto) {
+        //TODO: save beer
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+        log.debug("Deleting a beer...");
     }
 }
---
+++ src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
@@ -0,0 +1,62 @@
+package guru.springframework.msscbrewery.web.controller;
+
+import guru.springframework.msscbrewery.services.CustomerService;
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("/api/v1/customer")
+@RestController
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/{custId}")
+    public ResponseEntity<CustomerDto> getCustomerById(@PathVariable("custId") UUID custId){
+
+        return new ResponseEntity<CustomerDto>(customerService.getCustomerById(custId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody @Valid CustomerDto customerDto){
+        CustomerDto savedDto = customerService.saveNewCustomer(customerDto);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Location", "/api/v1/customer/" + savedDto.getId().toString());
+
+        return new ResponseEntity(httpHeaders, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{customerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void handleUpdate(@PathVariable("customerId") UUID customerId, @Valid @RequestBody CustomerDto customerDto){
+        customerService.updateCustomer(customerId, customerDto);
+    }
+
+    @DeleteMapping("/{customerId}")
+    public void deleteById(@PathVariable("customerId")  UUID customerId){
+        customerService.deleteById(customerId);
+    }
+
+    // Not need as added global exception handler under MvcExceptionHandler
+    /*@ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException e){
+        List<String> errors = new ArrayList<>(e.getConstraintViolations().size());
+
+        e.getConstraintViolations().forEach(constraintViolation -> {
+            errors.add(constraintViolation.getPropertyPath() + " : " + constraintViolation.getMessage());
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }*/
+}
